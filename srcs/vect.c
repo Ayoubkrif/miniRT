1efb44fa1528aa10b34a5efcf67b0acce3fdab0d
Ayoub Krifa <krifa.ayoub@yahoo.com>
2025-06-04 19:06:28 +0200

    vec_add

--- srcs/vect.c
+++ srcs/vect.c
@@ -6,17 +6,26 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 18:45:57 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/06/04 18:59:59 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/06/04 19:06:16 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_vec3	cross_product(t_vec3 vec1, t_vec3 vec2)
+t_vec3	cross_product(t_vec3 u, t_vec3 v)
 {
 	return ((t_vec3){
-		vec1.y * vec2.z - vec1.z * vec2.y,
-		vec1.z * vec2.x - vec1.x * vec2.z,
-		vec1.x * vec2.y - vec1.y * vec2.x,
+		.x = u.y * v.z - u.z * v.y,
+		.y = u.z * v.x - u.x * v.z,
+		.z = u.x * v.y - u.y * v.x,
+	});
+}
+
+t_vec3	vect_add(t_vec3 u, t_vec3 v)
+{
+	return ((t_vec3){
+		.x = u.x + v.x,
+		.y = u.y + v.y,
+		.z = u.z + v.z,
 	});
 }
