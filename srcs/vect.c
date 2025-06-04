35ab729ebca80cbbb20f5d9cbc39a5e3676759c7
Ayoub Krifa <krifa.ayoub@yahoo.com>
2025-06-04 19:00:12 +0200

    wtf

--- srcs/vect.c
+++ srcs/vect.c
@@ -6,7 +6,7 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 18:45:57 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/06/04 18:54:22 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/06/04 18:59:59 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 t_vec3	cross_product(t_vec3 vec1, t_vec3 vec2)
 {
-	t_vec3	result;
-
-	result.x = vec1.y * vec2.z - vec1.z * vec2.y;
-	result.y = vec1.z * vec2.x - vec1.x * vec2.z;
-	result.z = vec1.x * vec2.y - vec1.y * vec2.x;
-	return (result);
+	return ((t_vec3){
+		vec1.y * vec2.z - vec1.z * vec2.y,
+		vec1.z * vec2.x - vec1.x * vec2.z,
+		vec1.x * vec2.y - vec1.y * vec2.x,
+	});
 }
