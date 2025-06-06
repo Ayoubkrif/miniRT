5a9cf20cffb450a0f53708d9d7ad2e0430bd1bad
Clara Bordeau <cbordeau@f4r2s7.clusters.42paris.fr>
2025-06-06 13:52:22 +0200

    objects structures

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 14:17:37 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/06/05 15:12:05 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/06/06 12:44:58 by cbordeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,63 @@ typedef struct s_mlx
 	void	*img;
 }	t_mlx;
 
+typedef struct s_rgb
+{
+	int	x;
+	int	y;
+	int	z;
+}	t_rgb;
+
+typedef struct oject
+{
+	int	type;
+}	t_object;
+
 typedef struct s_camera
 {
 	t_vec3	position;
 	t_vec3	direction;
 	int		fov;
 }	t_camera;
+
+typedef struct s_ambient
+{
+	float	lighting;
+	t_rgb	color;
+}	t_ambient;
+
+typedef struct light
+{
+	t_vec3	position;
+	float	brightness;
+	t_rgb	color;
+}	t_light;
+
+typedef struct s_sphere
+{
+	int		type;
+	t_vec3	center;
+	float	diameter;
+	t_rgb	color;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	int		type;
+	t_vec3	point;
+	t_vec3	normal_v;
+	t_rgb	color;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	int		type;
+	t_vec3	center;
+	t_vec3	axis;
+	float	diameter;
+	float	height;
+	t_rgb	color;
+}	t_cylander;
 
 typedef struct s_rt
 {
