e7119f48aa6345c5b1362b630f5aee64bd4b9926
Ayoub Krifa <krifa.ayoub@yahoo.com>
2025-06-17 23:30:02 +0200

    first shadows

--- srcs/ray_casting.c
+++ srcs/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 10:58:38 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/06/17 18:45:19 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/06/17 22:55:43 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,12 @@ t_inter	add_inter(t_rt *rt, t_vect ray)
 t_rgb	shaker_ambiant_solid(t_rt *rt, t_rgb color, t_rgb diffuse)
 {
 	(void)rt;
+	(void)diffuse;
 	return ((t_rgb)
 		{
-			color.r * /* ((rt->ambiant.color.r * rt->ambiant.color.brightness) + */(diffuse.brightness * diffuse.r),
-			color.g * /* ((rt->ambiant.color.g * rt->ambiant.color.brightness) + */(diffuse.brightness * diffuse.g),
-			color.b * /* ((rt->ambiant.color.b * rt->ambiant.color.brightness) + */(diffuse.brightness * diffuse.b),
+			color.r *  ((rt->ambiant.color.r * rt->ambiant.color.brightness) + (diffuse.brightness * diffuse.r)),
+			color.g *  ((rt->ambiant.color.g * rt->ambiant.color.brightness) + (diffuse.brightness * diffuse.g)),
+			color.b *  ((rt->ambiant.color.b * rt->ambiant.color.brightness) + (diffuse.brightness * diffuse.b)),
 			1
 		});
 }
@@ -71,7 +72,7 @@ t_rgb	is_it_touching(t_rt *rt, double x, double y)
 	if (vect_eq(inter_obj_cam, inter_obj_light))
 		diffuse.color = rt->light.color;
 	else
-		diffuse.color = (t_rgb){0, 0, 0, 0};
+		diffuse.color = (t_rgb){1, 1, 1, 1};
 	return (shaker_ambiant_solid(rt, inter_ray_obj.color, diffuse.color));
 }
 
