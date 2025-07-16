a1d8c361c60125c9080b23da717217e5e6fbd07a
Clara Bordeau <cbordeau@f2r9s15.clusters.42paris.fr>
2025-07-16 18:22:35 +0200

    parsing bonus

--- srcs/init/constructors_bonus.c
+++ srcs/init/constructors_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cbordeau <cbordeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 11:52:14 by cbordeau          #+#    #+#             */
-/*   Updated: 2025/07/16 17:39:59 by cbordeau         ###   ########.fr       */
+/*   Updated: 2025/07/16 18:20:19 by cbordeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,95 @@
 #include "mlx.h"
 #include <stdio.h>
 
-int	obj_bonus(t_rt *rt, t_obj *obj, char **tok)
+int	fill_texture(t_rt *rt, t_obj *obj, char *tok)
 {
-	obj->map = 0;
-	if (tok[1])
-		obj->reflexion = atof(tok[1]);
-	else
-		return (print_error(REFLEXION, "sphere"));
-	obj->texture.img = NULL;
-	if (tok[2] && *tok[2] != '\n')
-	{
-		if (!ft_strcmp(tok[2], "checker"))
-			obj->map += 1;
-		else
-		{
-			obj->texture.img = mlx_xpm_file_to_image(rt->mlx.disp, tok[2], &obj->texture.width, &obj->texture.height);
-			if (!obj->texture.img)
-				printf("i dont have a xpm\n");
-			obj->map += 1;
-		}
-	}
+	tok += 2;
+	if (!ft_strcmp(tok, "checker"))
+		obj->map += 1;
 	else
 	{
-		return (0);
-	}
-	obj->bump.img = NULL;
-	if (tok[3] && *tok[3] != '\n')
-	{
-		obj->bump.img = mlx_xpm_file_to_image(rt->mlx.disp, tok[3], &obj->bump.width, &obj->bump.height);
+		obj->texture.img = mlx_xpm_file_to_image(rt->mlx.disp, tok, &obj->texture.width, &obj->texture.height);
 		if (!obj->texture.img)
-			return (printf("i dont have a bump on sp\n"), 1);
-		obj->map += 3;
+			return (printf("i dont have a xpm\n"), 1);
+		obj->map += 1;
 	}
 	return (0);
 }
+
+int	fill_bump(t_rt *rt, t_obj *obj, char *tok)
+{
+	*tok += 2;
+	obj->bump.img = mlx_xpm_file_to_image(rt->mlx.disp, tok, &obj->bump.width, &obj->bump.height);
+	if (!obj->texture.img)
+		return (printf("i dont have a bump on sp\n"), 1);
+	obj->map += 3;
+	return (0);
+}
+
+int	obj_bonus(t_rt *rt, t_obj *obj, char **tok)
+{
+	int	i;
+
+	i = -1;
+	obj->map = 0;
+	obj->texture.img = NULL;
+	obj->bump.img = NULL;
+	while (tok[i])
+	{
+		if (!ft_strncmp("r:", tok[i], 2))
+		{
+			*tok += 2;
+			obj->reflexion = atof(tok[i]);
+		}
+		if (!ft_strncmp("t:", tok[i], 2))
+		{
+			if (fill_texture(rt, obj, tok[i]))
+				return (printf("Error on texture\n"), 1);
+		}
+		if (!ft_strncmp("b:", tok[i], 2))
+		{
+			if (fill_bump(rt, obj, tok[i]))
+				return (printf("Error on texture\n"), 1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+// int	save_bonus(t_rt *rt, t_obj *obj, char **tok)
+// {
+// 	obj->map = 0;
+// 	if (tok[1])
+// 		obj->reflexion = atof(tok[1]);
+// 	else
+// 		return (print_error(REFLEXION, "sphere"));
+// 	obj->texture.img = NULL;
+// 	if (tok[2] && *tok[2] != '\n')
+// 	{
+// 		if (!ft_strcmp(tok[2], "checker"))
+// 			obj->map += 1;
+// 		else
+// 		{
+// 			obj->texture.img = mlx_xpm_file_to_image(rt->mlx.disp, tok[2], &obj->texture.width, &obj->texture.height);
+// 			if (!obj->texture.img)
+// 				printf("i dont have a xpm\n");
+// 			obj->map += 1;
+// 		}
+// 	}
+// 	else
+// 	{
+// 		return (0);
+// 	}
+// 	obj->bump.img = NULL;
+// 	if (tok[3] && *tok[3] != '\n')
+// 	{
+// 		obj->bump.img = mlx_xpm_file_to_image(rt->mlx.disp, tok[3], &obj->bump.width, &obj->bump.height);
+// 		if (!obj->texture.img)
+// 			return (printf("i dont have a bump on sp\n"), 1);
+// 		obj->map += 3;
+// 	}
+// 	return (0);
+// }
 
 int	sp_bonus(t_rt *rt, t_sp *sp, char **tok)
 {
