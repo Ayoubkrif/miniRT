d54be1ed21762d2aa28d7abf05d87ed4623f7f41
Aykrifa <aykrifa@student.42.fr>
2025-07-22 13:26:40 +0200

    split phong into blinnphong and cast_ray from

---
+++ srcs/intersection/blinn-phong.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phong.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 12:27:40 by aykrifa           #+#    #+#             */
+/*   Updated: 2025/07/22 13:23:16 by aykrifa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+#include "define.h"
+#include "vect.h"
+#include <math.h>
+
+void	add_lights(t_rgb *light_color, t_rgb diffuse, t_rgb specular)
+{
+	*light_color = color_add(*light_color, color_add(diffuse, specular));
+}
+
+t_rgb	diffuse_color(t_light light, double dot_normal_light)
+{
+	float	kd;
+
+	kd = KD * dot_normal_light;
+	return (color_k(light.color, kd));
+}
+
+t_rgb	specular_color(t_phong *phong, t_light light, t_vect h)
+{
+	float	ks;
+
+	ks = KS * fabs(pow(dot(phong->normal_n, h), ALPHA_S));
+	return (color_k(light.color, ks));
+}
+
+void	blin_phong(t_rt *rt, t_vect ray, t_phong *phong, t_rgb *lights)
+{
+	int		i;
+	t_vect	ray_obj_n;
+	t_vect	hit_light;
+	double	dot_normal_light;
+	t_inter	inter;
+
+	i = 0;
+	while (i < rt->nb_light)
+	{
+		ray_obj_n = normalize(vec_sub(phong->hit_ray, rt->light[i].position));
+		inter = nearest_inter(rt, ray_obj_n, rt->light[i].position);
+		hit_light = get_point(rt->light[i].position, ray_obj_n, inter.t);
+		if (vect_eq(&phong->hit_ray, &hit_light))
+		{
+			dot_normal_light = -dot(ray_obj_n, phong->normal_n);
+			if (dot_normal_light > 0)
+				add_lights(lights,
+					diffuse_color(rt->light[i], dot_normal_light),
+					specular_color(phong, rt->light[i],
+						normalize(vec_add(ray_obj_n, ray))));
+		}
+		i++;
+	}
+}
---
+++ srcs/intersection/cast_ray_from.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cast_ray_from.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 12:27:40 by aykrifa           #+#    #+#             */
+/*   Updated: 2025/07/22 13:23:08 by aykrifa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "math_utils.h"
+#include "miniRT.h"
+#include "vect.h"
+#include "math_utils.h"
+
+t_rgb	reflect_phong(t_rt *rt, t_vect ray, t_phong *phong, int precision)
+{
+	t_rgb	lights;
+	t_rgb	reflected;
+	float	reflexion;
+
+	reflexion = phong->inter_ray.obj->reflexion;
+	reflected = (t_rgb){0, 0, 0};
+	lights = (t_rgb){0, 0, 0};
+	if (reflexion > EPSILON && precision)
+		reflected = cast_ray_from(rt, phong->reflected,
+				vec_add(phong->hit_ray,
+					vec_mul(phong->reflected, EPSILON)), precision - 1);
+	if (!(reflexion > 1 - EPSILON && precision))
+		blin_phong(rt, ray, phong, &lights);
+	lights = color_add(rt->ambient.color, lights);
+	return (
+		color_add(
+			color_k(mul_rgb(phong->solid_color, lights), 1 - reflexion)
+			, color_k(reflected, reflexion)));
+}
+
+t_rgb	cast_ray_from(t_rt *rt, t_vect ray, t_vect from, int precision)
+{
+	t_phong	phong;
+
+	normalize_to(&ray);
+	phong.inter_ray = nearest_inter(rt, ray, from);
+	if (!phong.inter_ray.obj)
+		return ((t_rgb){0, 0, 0});
+	phong.hit_ray = get_point(from, ray, phong.inter_ray.t);
+	bump_texture(&phong.inter_ray, &phong.normal_n, &phong.solid_color,
+		&phong.hit_ray);
+	phong.dot_normal_ray = -dot(ray, phong.normal_n);
+	if (phong.dot_normal_ray < 0)
+	{
+		phong.normal_n = vec_mul(phong.normal_n, -1);
+		phong.dot_normal_ray *= -1;
+	}
+	phong.reflected = vec_sub(vec_mul(phong.normal_n, 2 * phong.dot_normal_ray),
+			vec_mul(ray, -1));
+	return (reflect_phong(rt, ray, &phong, precision));
+}
